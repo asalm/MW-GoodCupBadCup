0894264c9400c61613e7d4db43cf4aaa81809b92
Noki <ndinh@stud.hs-bremen.de>
2016-06-10 13:31:36 +0200

    Bugfixes mit der Kamera
    This reverts commit 5ef2bff254cd71618f8daa15de7ea7c00000d153.

--- foerderband/Assets/scripts/CupCounter.cs
+++ foerderband/Assets/scripts/CupCounter.cs
@@ -18,14 +18,9 @@ public class CupCounter : MonoBehaviour {
 		if (GameObject.Find("Treadmill").GetComponent<patternDrop>().level == counter)
 		{
 			counter = 0;
-			GameObject.Find ("Treadmill").GetComponent<patternDrop> ().roundInSeconds = 0;
 			GameObject.Find("Treadmill").GetComponent<patternDrop>().pattern.Clear();
 			GameObject.Find("Treadmill").GetComponent<patternDrop>().alreadyExists = false;
 			GameObject.Find("Treadmill").GetComponent<patternDrop>().spawnPattern();
 		}
-	}
-
-	public void setCounter(int c){
-		this.counter = c;
 	}
 }
